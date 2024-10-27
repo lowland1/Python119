8729847f91e04d30ff971b710fc851fd71261be1
lowland1 <160879623+lowland1@users.noreply.github.com>
2024-10-27 15:06:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import turtle as trtl
 runner = trtl.Turtle()
 rock = trtl.Turtle()
 runner.register_shape("runner.png")
+rock.register_shape("rock.png")
+runner.shape("runner.png")
+rock.shape("rock.png")
  
 wn = trtl.Screen()
 wn.mainloop()
