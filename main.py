cc77a31c801753b7809755b92445af3542e342c8
lowland1 <160879623+lowland1@users.noreply.github.com>
2024-10-27 17:52:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import turtle as trtl
 import keyboard
 import random
-
+# Set up the game
 runner = trtl.Turtle()
 rock = trtl.Turtle()
+runner.speed(3)
+runner.penup()
+rock.penup()
+#set positions
+runner.left(90)
+runner.goto(-100,0)
+rock.goto(100,0)
+# set image of runner and rock
 runner.register_shape("runner.png")
 rock.register_shape("rock.png")
 runner.shape("runner.png")
 rock.shape("rock.png")
 
 
-def jump() :
- 
- 
+
+#FUNCTIONS
+# Jump function
+def jump():
+ runner.forward(25)
+ runner.backward(25)
+
 wn = trtl.Screen()
 wn.mainloop()
