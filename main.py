10459b0f0fe00e00209e1755399487ac6f03d276
lowland1 <160879623+lowland1@users.noreply.github.com>
2024-10-28 13:48:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,67 @@
 import turtle as trtl
-import keyboard
 import random
 # Set up the game
+score = 0
+score_board = []
+wn = trtl.Screen()
+wn.register_shape("runner.gif")
+wn.register_shape("rock.gif")
 runner = trtl.Turtle()
 rock = trtl.Turtle()
-runner.speed(3)
-runner.penup()
+score_trtl = trtl.Turtle()
+score_trtl.hideturtle()
+runner.speed(0)
+score_trtl.pensize(10)
 rock.penup()
+runner.penup()
+score_trtl.penup()
 #set positions
-runner.left(90)
-runner.goto(-100,0)
-rock.goto(100,0)
+runner.goto(-400,50)
+rock.left(180)
+rock.goto(450,0)
+score_trtl.goto(0,350)
+score_trtl.pendown()
 # set image of runner and rock
-runner.register_shape("runner.png")
-rock.register_shape("rock.png")
-runner.shape("runner.png")
-rock.shape("rock.png")
-
-
+runner.shape("runner.gif")
+rock.shape("rock.gif")
 
 #FUNCTIONS
-# Jump function
-def jump():
- runner.forward(25)
- runner.backward(25)
+# Game over function
+def game_over():
+ global score
+ global score_board
+ score_trtl.write("GAME OVER, CURRENT SCORE IS:" + score + ", HIGH SCORE IS:" )
+ score_board.append(score)
 
-wn = trtl.Screen()
+
+# Jump function
+def jump(x,y):
+ runner.sety(250)
+ wn.ontimer(runner.sety(50),200)
+
+# moving rock function
+def move():
+ rock.goto(rock.xcor()-50,0)
+ if rock.distance(runner) < 10:
+  game_over()
+ if rock.xcor()< -500:
+  rock.setx(450)
+ wn.ontimer(move, 50)
+
+
+#score function
+def add_score():
+ global score
+ score += 1000
+ score_trtl.clear()
+ score_trtl.write(score, font='Arial', align='center')
+ wn.ontimer(add_score,1000)
+
+
+#CODE
+wn.onclick(jump)
+wn.ontimer(add_score,1000)
+move()
+
+ 
 wn.mainloop()
